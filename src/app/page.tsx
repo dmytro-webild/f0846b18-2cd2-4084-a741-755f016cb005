89addb6dfd68aa9bc7a0f3425618e372cf12afba
bender <kudin.dima@gmail.com>
2026-05-05 02:12:48 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function LandingPage() {
         { name: "Trust", id: "trust" },
         { name: "Contact", id: "contact" },
       ]}
-      brandName="Simon Appliance & Plumbing Ltd"
+      brandName="Simon Plumbing "
       logoClassName="w-48 h-auto scale-150"
       button={{ text: "Get Quote", href: "#contact" }}
     />
